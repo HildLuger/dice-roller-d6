d6c2aa3ed9ca367f20aac79539b7b15319a2b123
HildLuger <88519194+HildLuger@users.noreply.github.com>
2022-09-25 17:14:34 -0300

    Add files via upload

---
+++ dice_v2/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/tUWe7fNJQTUo9x3h/scene.splinecode');
